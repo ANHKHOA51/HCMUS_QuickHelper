6483d5de3aa882f50fa76e5d9494f34a9a0a0523
lykhai2512 <161923199+lykhai2512@users.noreply.github.com>
2026-04-02 13:04:07 +0700

    Regis UI only

--- app/src/main/java/com/example/hcmus_quickhelper/features/auth/datasource/AuthRemoteDataSource.kt
+++ app/src/main/java/com/example/hcmus_quickhelper/features/auth/datasource/AuthRemoteDataSource.kt
@@ -3,12 +3,25 @@ package com.example.hcmus_quickhelper.features.auth.datasource
 import com.example.hcmus_quickhelper.core.database.SupabaseClient
 import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.auth.providers.builtin.Email
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
 class AuthRemoteDataSource {
     suspend fun loginWithEmail(email: String, pass: String) {
         SupabaseClient.client.auth.signInWith(Email) {
             this.email = email
             this.password = pass
+        }
+    }
+
+    suspend fun registerWithEmail(email: String, pass: String, fullname: String, phone: String) {
+        SupabaseClient.client.auth.signUpWith(Email) {
+            this.email = email
+            this.password = pass
+            data = buildJsonObject {
+                put("full_name", fullname)
+                put("phone", phone)
+            }
         }
     }
 }
