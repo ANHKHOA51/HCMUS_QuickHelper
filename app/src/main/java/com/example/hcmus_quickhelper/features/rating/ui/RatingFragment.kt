18532b2d0c4116effa9eef58c1fa8d994d9a2e52
hlam3004 <hlam.30042005@gmail.com>
2026-04-23 13:36:01 +0700

    update rating

--- app/src/main/java/com/example/hcmus_quickhelper/features/rating/ui/RatingFragment.kt
+++ app/src/main/java/com/example/hcmus_quickhelper/features/rating/ui/RatingFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.hcmus_quickhelper.R
 import com.example.hcmus_quickhelper.databinding.FragmentRatingBinding
 import com.example.hcmus_quickhelper.databinding.FragmentReceiptBinding
@@ -89,6 +91,9 @@ class RatingFragment : Fragment(R.layout.fragment_rating) {
         val comment = binding.etComment.text.toString()
 
         viewModel.submitRating(point, comment)
+        Toast.makeText(requireContext(), "Cảm ơn bạn đã đánh giá dịch vụ!", android.widget.Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_rating_fragment_to_home_fragment)
+
     }
 
     override fun onDestroyView() {
